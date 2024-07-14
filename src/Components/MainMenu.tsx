eaf89201ccf760dabe6326ea6ecba9e9bc559b6d
Abduquddus <33756692+Abduquddus1998@users.noreply.github.com>
2024-07-14 13:44:01 +0100

    minor changes in MainMenu component

--- src/Components/MainMenu.tsx
+++ src/Components/MainMenu.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import {
   MenuContainer,
@@ -12,6 +12,7 @@ import { HistoryItem, useHistory } from "../hooks/analysis";
 
 export default function MainMenu() {
   const navigate = useNavigate();
+  const location = useLocation();
   const { id } = useParams<{ id: string | undefined }>();
 
   const { isPending, isError, data } = useHistory();
@@ -20,9 +21,18 @@ export default function MainMenu() {
     navigate(`/main/analysis/${history.corpus_id}`);
   };
 
+  const onNavigateMain = () => {
+    if (location.pathname === "/main") {
+      window.location.href = "/main";
+      return;
+    }
+
+    navigate(`/main`);
+  };
+
   return (
     <MenuContainer>
-      <MenuHeader onClick={() => navigate(`/main`)}>
+      <MenuHeader onClick={onNavigateMain}>
         <div>Create new tab</div>
         <CreateAnalysis />
       </MenuHeader>
