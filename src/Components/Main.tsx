f45b0b34c2d51a061637bdd004fe1c1a519377c7
Abduquddus <33756692+Abduquddus1998@users.noreply.github.com>
2024-07-16 17:57:08 +0100

    Main component refactored

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,47 +1,26 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 
-import Tabs from "./Tabs";
-import Table from "./Table";
 import { Spinner, SubmitButton } from "./LoginStyles";
 import { AnalyzeWrapper, SentimentWrapper } from "./SentimentAnalysisStyle";
 
-import {
-  KeyPhrases,
-  PiiLabels,
-  SentimentResponse,
-  useSentimentGenerator,
-} from "../hooks/analysis";
+import { useSentimentGenerator } from "../hooks/analysis";
 
 export default function Main() {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const mutation = useSentimentGenerator();
 
-  const [currentTab, setCurrentTab] = useState<number>(0);
   const [corpus, setCorpus] = useState("");
-  const [sentimentInfo, setSentimentInfo] = useState<SentimentResponse>({
-    corpus: "",
-    corpus_id: "",
-    prob: 0,
-    sentiment: "",
-    pii_labels: [],
-    ner_labels: [],
-    pos_labels: [],
-    key_phrases: [],
-  });
-
-  const columns: string[] = ["token", "label", "prob"];
-
-  const onSelectTab = (tab: number) => {
-    setCurrentTab(tab);
-  };
 
   const onSubmit = async () => {
     const { data, error } = await mutation.mutateAsync({ corpus });
 
     if (!error && data) {
       await queryClient.invalidateQueries({ queryKey: ["historyList"] });
-      setSentimentInfo(data);
+
+      navigate(`/main/analysis/${data.corpus_id}`);
     }
   };
 
@@ -56,60 +35,10 @@ export default function Main() {
       />
 
       <AnalyzeWrapper>
-        <SubmitButton
-          onClick={onSubmit}
-          isLogin={true}
-          disabled={!!mutation.data}
-        >
+        <SubmitButton onClick={onSubmit} isLogin={true} disabled={!corpus}>
           {mutation.isPending ? <Spinner /> : <>Analyze</>}
         </SubmitButton>
       </AnalyzeWrapper>
-
-      {!mutation.isPending && mutation.data && (
-        <Fragment>
-          <Tabs currentTab={currentTab} onSelectTab={onSelectTab} />
-
-          {currentTab === 0 && (
-            <Table<{ sentiment: string; prob: number }>
-              columns={["sentiment", "prob"]}
-              rows={[
-                {
-                  sentiment: sentimentInfo.sentiment,
-                  prob: sentimentInfo.prob,
-                },
-              ]}
-            />
-          )}
-
-          {currentTab === 1 && (
-            <Table<PiiLabels>
-              columns={columns}
-              rows={sentimentInfo.ner_labels}
-            />
-          )}
-
-          {currentTab === 2 && (
-            <Table<PiiLabels>
-              columns={columns}
-              rows={sentimentInfo.pii_labels}
-            />
-          )}
-
-          {currentTab === 3 && (
-            <Table<PiiLabels>
-              columns={columns}
-              rows={sentimentInfo.pos_labels}
-            />
-          )}
-
-          {currentTab === 4 && (
-            <Table<KeyPhrases>
-              columns={["phrase", "score"]}
-              rows={sentimentInfo.key_phrases}
-            />
-          )}
-        </Fragment>
-      )}
     </SentimentWrapper>
   );
 }
